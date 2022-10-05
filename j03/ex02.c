412ef900c662d07fe7d2cf53b7c275147dd02b7b
Younes-t <younes3046@gmail.com>
2022-10-05 10:59:04 -0400

    j03 - ex01/ex02 corrigés

--- j03/ex02.c
+++ j03/ex02.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int **mat2(int n, int m);
+
 int main(){
-    int *matrix=mat(3,4);
+    //int *matrix=mat(3,4);
+    int **matrix=mat2(4,4);
+    //printf("%d",matrix[0][0]);
+    printMat(matrix,4,4);
     return (0);
 }
 
+//without malloc
 int mat(int n, int m){
     int mat[n][m];
 
@@ -18,12 +24,28 @@ int mat(int n, int m){
     return (mat);
 }
 
+//with malloc
+int **mat2(int n, int m){
+    int **mat=(int *)malloc(sizeof(int)*n);
 
-/*void printMat(int mat, int n, int m){
+    for(int l=0;l<n;l++){
+        mat[l]=(int *)malloc(sizeof(int)*m);
+        for(int c=0;c<m;c++){
+            //printf("%d/%d\n",l,c);
+            if(l>c){mat[l][c]=l;}
+            else{mat[l][c]=c;}
+        }
+    }
+    return (mat);
+
+}
+
+
+void printMat(int **mat, int n, int m){
     for(int l=0;l<n;l++){
         printf("\n");
         for(int c=0;c<m;c++){
             printf("%d",mat[l][c]);
         }
     }
-}*/
+}
