bb27d6d49f66aefb2947edbfd79d956ca36cb90e
Younes-t <younes3046@gmail.com>
2022-10-06 03:38:06 -0400

    j02 - ex04 corrigé

--- j02/ex04.c
+++ j02/ex04.c
@@ -4,29 +4,29 @@
 void o_reverse_int_table(int *arr, int size);
 
 int main(){
-    int lst[]={2,3,4};
-    printArray(lst,3);
-    o_reverse_int_table(lst,3);
-    printArray(lst,3);
+    int lst[]={10,9,8,7,6,5,4,3,2,1};
+    printArray(lst,10);
+    o_reverse_int_table(lst,10);
+    printArray(lst,10);
     return (0);
 }
 
 void o_reverse_int_table(int *arr, int size){
-    size=size-1;
-    int buff[size];
-    int n=0;
-    for(int i=size;i>=0;i--){
-        buff[n]=arr[i];
-        n++;
+    int buff,taille;
+    if(size%2==0){taille=size/2;}
+    else{taille=(size/2)+1;}
+
+    for(int i=0;i<taille;i++){
+        buff=arr[i];
+        arr[i]=arr[(size-1)-i];
+        arr[(size-1)-i]=buff;
     }
-    for(int i=0;i<size;i++){
-        arr[i]=buff[i];
-    }
+  
 }
 
 void printArray(int arr[], int size){
     for(int i=0;i<size;i++){
-        printf("%d",arr[i]);
+        printf("%d ",arr[i]);
     }
     printf("\n");
 }
